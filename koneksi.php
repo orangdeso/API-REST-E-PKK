15e311bf0685fb583cc668d17250f5d27cea83f3
Daffa Aditya Rejasa Ruswanto <112993458+Daffaaditya2807@users.noreply.github.com>
2023-06-13 00:07:53 +0700

    Update rest api

--- koneksi.php
+++ koneksi.php
@@ -5,6 +5,6 @@ $cek = null;
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "pkk_nganjuk";
+$db = "db_pkk2";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db) or die("Database MYSQL Tidak Terhubung");
